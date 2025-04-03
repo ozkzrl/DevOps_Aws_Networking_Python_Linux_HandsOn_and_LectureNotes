227cafafd270336aebbeb66b991a766d849493ff
ozkzrl <ozkan.zorlu@gmail.com>
2025-04-03 10:21:26 +0300

    03.04.2025 Lessons new lessons notes were added.

---
+++ DevOps/AWS/projects/flask-_01_02_Jinja_Template/solution/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,23 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+   return "Hello World Paul"
+
+@app.route('/second')
+def second():
+   return "This is second Page"
+
+@app.route('/third')
+def third():
+   return "This is third Page"
+
+@app.route('/fourth/<string:id>')
+def fourth(id):
+   return f'Id of this page {id}'
+
+
+if __name__ == '__main__':
+   # app.run(debug=True, port=3000)
+   app.run(host= '0.0.0.0', port=8080)
