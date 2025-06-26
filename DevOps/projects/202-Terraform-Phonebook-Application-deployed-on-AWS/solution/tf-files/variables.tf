179978cf373cc8a5b71051eb267419d4e368b8b1
oguzhan fidan <oguzhan@talkshop.live>
2025-06-26 19:18:52 +0300

    tf prj solution added

---
+++ DevOps/projects/202-Terraform-Phonebook-Application-deployed-on-AWS/solution/tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "ofidan"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "oliver"
+}
+
+variable "hosted-zone" {
+  default = "example.com"
+}
