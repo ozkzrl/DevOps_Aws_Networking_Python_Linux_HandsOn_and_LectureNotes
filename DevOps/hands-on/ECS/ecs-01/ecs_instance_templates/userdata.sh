ea828b1106c87bf743aec6b18e97882aa7949d5a
David <67348896+davidinst@users.noreply.github.com>
2025-07-23 13:53:24 +0300

    Update userdata.sh

--- DevOps/hands-on/ECS/ecs-01/ecs_instance_templates/userdata.sh
+++ DevOps/hands-on/ECS/ecs-01/ecs_instance_templates/userdata.sh
@@ -4,5 +4,5 @@ dnf install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
-curl -SL https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.38.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
